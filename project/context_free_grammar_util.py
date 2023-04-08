c1a3fbb0950d7fd3a0504e13194f3be94b41d890
Dmitrii.Karagodin <4319788@gmail.com>
2023-04-08 14:05:19 +0300

    Task 7 - RecursiveFiniteAutomata returns AdjacencyMatrices. Refactoring. Some functions moved to classes and turned into static methods

--- project/context_free_grammar_util.py
+++ project/context_free_grammar_util.py
@@ -1,4 +1,8 @@
+from typing import Dict
+
 from pyformlang.cfg import CFG, Variable
+from pyformlang.regular_expression import Regex
+from project.ecfg_util import ECFG
 
 
 def cfg_to_weak_cnf(cfg: CFG) -> CFG:
@@ -21,3 +25,29 @@ def get_cfg_from_file(file: str, start_symbol: Variable = Variable("S")) -> CFG:
     """
     with open(file) as f:
         return CFG.from_text(f.read(), start_symbol=start_symbol)
+
+
+def convert_cfg_to_ecfg(cfg: CFG) -> ECFG:
+    """
+    Converts CFG to ECFG
+    :param cfg: CFG
+    :return: ECFG
+    """
+    productions: Dict[Variable, Regex] = dict()
+    for prod in cfg.productions:
+        lst = productions.setdefault(prod.head.value, [])
+        if prod.body:
+            lst.append(".".join(str(e.value) for e in prod.body))
+        else:
+            lst.append("$")
+
+    productions = {
+        head: Regex(" | ".join(f"({elem})" for elem in body))
+        for head, body in productions.items()
+    }
+    return ECFG(
+        start_symbol=cfg.start_symbol,
+        variables=cfg.variables,
+        productions=productions
+    )
+
