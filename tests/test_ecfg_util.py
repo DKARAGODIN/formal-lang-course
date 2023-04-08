30d665821cd5e2d6a36445eb877e4962160c009b
Dmitrii.Karagodin <4319788@gmail.com>
2023-04-08 14:05:19 +0300

    Task 7 - RecursiveFiniteAutomata returns AdjacencyMatrices. Refactoring. Some functions moved to classes and turned into static methods

--- tests/test_ecfg_util.py
+++ tests/test_ecfg_util.py
@@ -3,8 +3,10 @@ import os
 
 from pyformlang.cfg import Variable, CFG
 from pyformlang.regular_expression import Regex
-from project.ecfg_util import get_ecfg_from_string, convert_ecfg_to_RecursiveFiniteAutomata, \
-    minimize_RecursiveFiniteAutomata, RecursiveFiniteAutomata, ECFG, convert_cfg_to_ecfg, get_ecfg_from_file
+
+from project.context_free_grammar_util import convert_cfg_to_ecfg
+from project.ecfg_util import RecursiveFiniteAutomata, ECFG
+from project.matrix_util import AdjacencyMatrix
 from project.regex_util import regex_to_min_dfa
 from tests.test_context_free_grammar_util import create_temp_file
 
@@ -42,32 +44,32 @@ class ECFGUtilTest(unittest.TestCase):
     #Group1
     def test_convert_ecfg_to_RecursiveFiniteAutomata_1(self):
         s = ""
-        ecfg = get_ecfg_from_string(s)
-        rfa = convert_ecfg_to_RecursiveFiniteAutomata(ecfg)
+        ecfg = ECFG.get_ecfg_from_string(text=s)
+        rfa = ecfg.convert_to_RecursiveFiniteAutomata()
         check_ecfg_and_rfa(ecfg, rfa)
 
     def test_convert_ecfg_to_RecursiveFiniteAutomata_2(self):
         s = "S -> x"
-        ecfg = get_ecfg_from_string(s)
-        rfa = convert_ecfg_to_RecursiveFiniteAutomata(ecfg)
+        ecfg = ECFG.get_ecfg_from_string(s)
+        rfa = ecfg.convert_to_RecursiveFiniteAutomata()
         check_ecfg_and_rfa(ecfg, rfa)
 
     def test_convert_ecfg_to_RecursiveFiniteAutomata_3(self):
-        ecfg_as_text = "S -> a | b* S"
-        ecfg = get_ecfg_from_string(ecfg_as_text)
-        rfa = convert_ecfg_to_RecursiveFiniteAutomata(ecfg)
+        s = "S -> a | b* S"
+        ecfg = ECFG.get_ecfg_from_string(s)
+        rfa = ecfg.convert_to_RecursiveFiniteAutomata()
         check_ecfg_and_rfa(ecfg, rfa)
 
     def test_convert_ecfg_to_RecursiveFiniteAutomata_4(self):
         s = "S -> epsilon"
-        ecfg = get_ecfg_from_string(s)
-        rfa = convert_ecfg_to_RecursiveFiniteAutomata(ecfg)
+        ecfg = ECFG.get_ecfg_from_string(s)
+        rfa = ecfg.convert_to_RecursiveFiniteAutomata()
         check_ecfg_and_rfa(ecfg, rfa)
 
     def test_convert_ecfg_to_RecursiveFiniteAutomata_5(self):
         s = "S -> epsilon | a S b S"
-        ecfg = get_ecfg_from_string(s)
-        rfa = convert_ecfg_to_RecursiveFiniteAutomata(ecfg)
+        ecfg = ECFG.get_ecfg_from_string(s)
+        rfa = ecfg.convert_to_RecursiveFiniteAutomata()
         check_ecfg_and_rfa(ecfg, rfa)
 
     # Group2
@@ -104,66 +106,118 @@ class ECFGUtilTest(unittest.TestCase):
     # Group3
     def test_minimize_RecursiveFiniteAutomata_1(self):
         s = ""
-        rfa = convert_ecfg_to_RecursiveFiniteAutomata(get_ecfg_from_string(s))
-        minimized_rfa = minimize_RecursiveFiniteAutomata(rfa)
+        rfa = ECFG.get_ecfg_from_string(s).convert_to_RecursiveFiniteAutomata()
+        minimized_rfa = rfa.minimize_RecursiveFiniteAutomata()
         check_minimize(minimized_rfa)
 
     def test_minimize_RecursiveFiniteAutomata_2(self):
         s = "S -> x"
-        rfa = convert_ecfg_to_RecursiveFiniteAutomata(get_ecfg_from_string(s))
-        minimized_rfa = minimize_RecursiveFiniteAutomata(rfa)
+        rfa = ECFG.get_ecfg_from_string(s).convert_to_RecursiveFiniteAutomata()
+        minimized_rfa = rfa.minimize_RecursiveFiniteAutomata()
         check_minimize(minimized_rfa)
 
     def test_minimize_RecursiveFiniteAutomata_3(self):
         s = "S -> a | b* S"
-        rfa = convert_ecfg_to_RecursiveFiniteAutomata(get_ecfg_from_string(s))
-        minimized_rfa = minimize_RecursiveFiniteAutomata(rfa)
+        rfa = ECFG.get_ecfg_from_string(s).convert_to_RecursiveFiniteAutomata()
+        minimized_rfa = rfa.minimize_RecursiveFiniteAutomata()
         check_minimize(minimized_rfa)
 
     def test_minimize_RecursiveFiniteAutomata_4(self):
         s = "S -> epsilon"
-        rfa = convert_ecfg_to_RecursiveFiniteAutomata(get_ecfg_from_string(s))
-        minimized_rfa = minimize_RecursiveFiniteAutomata(rfa)
+        rfa = ECFG.get_ecfg_from_string(s).convert_to_RecursiveFiniteAutomata()
+        minimized_rfa = rfa.minimize_RecursiveFiniteAutomata()
         check_minimize(minimized_rfa)
 
     def test_minimize_RecursiveFiniteAutomata_5(self):
         s = "S -> epsilon | a S b S"
-        rfa = convert_ecfg_to_RecursiveFiniteAutomata(get_ecfg_from_string(s))
-        minimized_rfa = minimize_RecursiveFiniteAutomata(rfa)
+        rfa = ECFG.get_ecfg_from_string(s).convert_to_RecursiveFiniteAutomata()
+        minimized_rfa = rfa.minimize_RecursiveFiniteAutomata()
         check_minimize(minimized_rfa)
 
     # Group4
     def test_get_ecfg_from_file_1(self):
         file = create_temp_file("ecfg.txt", "")
-        ecfg = get_ecfg_from_file(file)
-        rfa = convert_ecfg_to_RecursiveFiniteAutomata(ecfg)
+        ecfg = ECFG.get_ecfg_from_file(file)
+        rfa = ecfg.convert_to_RecursiveFiniteAutomata()
         check_ecfg_and_rfa(ecfg, rfa)
         os.remove(file)
 
     def test_get_ecfg_from_file_2(self):
         file = create_temp_file("ecfg.txt", "S -> x")
-        ecfg = get_ecfg_from_file(file)
-        rfa = convert_ecfg_to_RecursiveFiniteAutomata(ecfg)
+        ecfg = ECFG.get_ecfg_from_file(file)
+        rfa = ecfg.convert_to_RecursiveFiniteAutomata()
         check_ecfg_and_rfa(ecfg, rfa)
         os.remove(file)
 
     def test_get_ecfg_from_file_3(self):
         file = create_temp_file("ecfg.txt", "S -> a | b* S")
-        ecfg = get_ecfg_from_file(file)
-        rfa = convert_ecfg_to_RecursiveFiniteAutomata(ecfg)
+        ecfg = ECFG.get_ecfg_from_file(file)
+        rfa = ecfg.convert_to_RecursiveFiniteAutomata()
         check_ecfg_and_rfa(ecfg, rfa)
         os.remove(file)
 
     def test_get_ecfg_from_file_4(self):
         file = create_temp_file("ecfg.txt", "S -> epsilon")
-        ecfg = get_ecfg_from_file(file)
-        rfa = convert_ecfg_to_RecursiveFiniteAutomata(ecfg)
+        ecfg = ECFG.get_ecfg_from_file(file)
+        rfa = ecfg.convert_to_RecursiveFiniteAutomata()
         check_ecfg_and_rfa(ecfg, rfa)
         os.remove(file)
 
     def test_get_ecfg_from_file_5(self):
         file = create_temp_file("ecfg.txt", "S -> epsilon | a S b S")
-        ecfg = get_ecfg_from_file(file)
-        rfa = convert_ecfg_to_RecursiveFiniteAutomata(ecfg)
+        ecfg = ECFG.get_ecfg_from_file(file)
+        rfa = ecfg.convert_to_RecursiveFiniteAutomata()
         check_ecfg_and_rfa(ecfg, rfa)
         os.remove(file)
+
+    # Group5
+    def test_am_1(self):
+        s = ""
+        ecfg = ECFG.get_ecfg_from_string(s)
+        rfa = ecfg.convert_to_RecursiveFiniteAutomata()
+        ams = rfa.get_adjacency_matrices()
+        assert ams == {}
+
+    def test_am_2(self):
+        s = "S -> x"
+        ecfg = ECFG.get_ecfg_from_string(s)
+        rfa = ecfg.convert_to_RecursiveFiniteAutomata()
+        ams = rfa.get_adjacency_matrices()
+        assert len(ams) == 1
+        am: AdjacencyMatrix = ams[Variable("S")]
+        assert am.get_states_len() == 2
+        assert am.get_start_states_len() == 1
+        assert am.get_final_states_len() == 1
+
+    def test_am_3(self):
+        s = "S -> a | b* S"
+        ecfg = ECFG.get_ecfg_from_string(s)
+        rfa = ecfg.convert_to_RecursiveFiniteAutomata()
+        ams = rfa.get_adjacency_matrices()
+        assert len(ams) == 1
+        am: AdjacencyMatrix = ams[Variable("S")]
+        assert am.get_states_len() == 4
+        assert am.get_start_states_len() == 1
+        assert am.get_final_states_len() == 2
+
+    def test_am_4(self):
+        s = "S -> epsilon"
+        ecfg = ECFG.get_ecfg_from_string(s)
+        rfa = ecfg.convert_to_RecursiveFiniteAutomata()
+        ams = rfa.get_adjacency_matrices()
+        assert len(ams) == 1
+        am: AdjacencyMatrix = ams[Variable("S")]
+        assert am.get_states_len() == 1
+        assert am.get_start_states_len() == 1
+        assert am.get_final_states_len() == 1
+
+    def test_am_5(self):
+        s = "S -> epsilon | a S b S"
+        ecfg = ECFG.get_ecfg_from_string(s)
+        rfa = ecfg.convert_to_RecursiveFiniteAutomata()
+        ams = rfa.get_adjacency_matrices()
+        assert len(ams) == 1
+        am: AdjacencyMatrix = ams[Variable("S")]
+        assert am.get_states_len() == 5
+        assert am.get_start_states_len() == 1
+        assert am.get_final_states_len() == 2
