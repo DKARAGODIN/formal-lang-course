b472e08f8c1b7327896f9871ea5d398aa8f1ae8c
Dmitrii.Karagodin <4319788@gmail.com>
2023-04-15 16:46:14 +0300

    Task 8 Hellings

--- project/rpq.py
+++ project/rpq.py
@@ -1,9 +1,12 @@
-from typing import Set, Dict, Iterable
+from collections import defaultdict
+from typing import Set, Dict, Iterable, Tuple
 
+from pyformlang.cfg import Terminal, CFG, Variable
 from scipy import sparse
 import networkx as nx
 
 import project.regex_util as regex_util
+from project.context_free_grammar_util import cfg_to_weak_cnf
 from project.matrix_util import AdjacencyMatrix, intersect_adjacency_matrices, _get_front, _get_reachable_states
 
 
@@ -122,3 +125,91 @@ def rpq_to_graph_bfs_all_reachable(
 
     return {start.value: {end.value for end in ends} for (start, ends) in result.items()}
 
+
+def context_free_path_query(graph: nx.MultiDiGraph, cfg: CFG,
+    start_nodes: Set = None, final_nodes: Set = None, start_symbol: Variable = Variable("S")
+) -> Set:
+    """
+    Executes query on graph with Hellings algorithm
+    :param graph: Graph as MultiDiGraph
+    :param cfg: CFG
+    :param start_nodes: Set of start nodes
+    :param final_nodes: Set of final nodes
+    :param start_symbol: Start symbol, defaults to "S"
+    :return: Pairs of vertices that have path between them with given constraints from graph
+    """
+    cfg._start_symbol = start_symbol
+
+    if start_nodes is None:
+        start_nodes = graph.nodes
+    if final_nodes is None:
+        final_nodes = graph.nodes
+
+    result = set()
+
+    for u, var_1, v in hellings(cfg, graph):
+        if start_symbol == var_1 and u in start_nodes and v in final_nodes:
+            result.add((u, v))
+    return result
+
+
+def hellings(cfg: CFG, graph: nx.MultiDiGraph) -> Set[Tuple]:
+    """
+    Calculate reachability between all pirs of vertices with Hellings algorithm on given CFG and graph
+    :param cfg: CFG
+    :param graph: Graph
+    :return: triplets (vertex, variable, vertex) Vertex - NonTerminal - Vertex
+    """
+    node_count = graph.number_of_nodes()
+    if node_count == 0:
+        return set()
+
+    wcnf = cfg_to_weak_cnf(cfg)
+    var_to_var_dict = defaultdict(set)
+    result = set()
+    queue = []
+
+    for productions in wcnf.productions:
+        terminal_to_variable = defaultdict(set)
+        eps = set()
+        l = len(productions.body)
+        if l == 0:
+            eps.add(productions.head)
+        elif l == 1:
+            terminal_to_variable[productions.head].add(productions.body[0])
+        elif l == 2:
+            var_to_var_dict[productions.head].add((productions.body[0], productions.body[1]))
+        for node in graph.nodes:
+            for var in eps:
+                triplet = (node, var, node)
+                result.add(triplet)
+                queue.append(triplet)
+        for i, j, label in graph.edges(data="label"):
+            for n, terms in terminal_to_variable.items():
+                if Terminal(label) in terms:
+                    triplet = (i, n, j)
+                    result.add(triplet)
+                    queue.append(triplet)
+
+    while len(queue) != 0:
+        u_1, var_1, v_1 = queue.pop(0)
+        diff = set()
+
+        for u_2, var_2, v_2 in result:
+            if v_2 == u_1:
+                for node_count, var_to_var in var_to_var_dict.items():
+                    triplet = (u_2, node_count, v_1)
+                    if (var_2, var_1) in var_to_var and triplet not in result:
+                        diff.add(triplet)
+                        queue.append(triplet)
+
+            if v_1 == u_2:
+                for node_count, var_to_var in var_to_var_dict.items():
+                    triplet = (u_1, node_count, v_2)
+                    if (var_1, var_2) in var_to_var and triplet not in result:
+                        diff.add(triplet)
+                        queue.append(triplet)
+
+        result = result.union(diff)
+
+    return result
